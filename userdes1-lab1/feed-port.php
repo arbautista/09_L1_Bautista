54f20045e290d4647287b04cd8a8b0216a4ded9a
arbautista <52182506+arbautista@users.noreply.github.com>
2019-07-15 18:04:24 -0700

    Add files via upload

---
+++ userdes1-lab1/feed-port.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Photog Feed</title>
+	<meta charset="UTF-8">
+	<link rel="icon" href="favicon.png">
+	<link rel="shortcut icon" href="favicon.png">
+	<link type="text/css"  rel="stylesheet"  href="style.css">
+</head>
+<body>
+	<header>
+	<div id="logo"><img src="logo.png" alt="logo"></div>
+		<nav>
+	        <ul>
+    	        <li><a href="index.html">H O M E</a></li>
+        	    <li><a href="about.html">A B O U T</a></li>
+            	<li><a href="photography.html">P H O T O G R P H Y</a></li>
+            	<li><a href="blog.html">B L O G S</a></li>
+            	<li><a href="contact.html">C O N T A C T</a></li>
+    	</nav>
+	</header>
+	<div class="clearfix"></div>
+		<main>
+			<!-- HOME-->
+				<section id="index.htm">
+						<h2>PORTFOLIO</h2>
+					
+				</section>
+			<!-- HEADER UNDER LOGO -->
+				<section class="span4 offset4">
+					<img src="divider_1.png" class="left span3 offset1">
+					<h1 class="left span5">FEED</h1>
+					<img src="divider_1.png" class="left span3">
+					<div class="clearfix"></div>
+					<p>
+					Photos taken from different places and event. Edited at Adobe Lightroom and Photoshoot. 
+				</p>
+				</section>
+			<!-- CONTENT 01 -->
+				<section class="offset2">
+				
+			<!--GALLERY -->
+						<div class="gallery">
+						  <a target="_blank" href="f1.jpg">
+						    <img src="f1.jpg" alt="calligraphy_01" width="300" height="200">
+						  </a>
+						  <div class="desc">'Eyes on the boat' Captured at Papaya Island, Batangas</div>
+						</div>
+
+						<div class="gallery">
+						  <a target="_blank" href="f5.jpg">
+						    <img src="f5.jpg" alt="calligraphy_02" width="300" height="200"></a>
+						  <div class="desc">'Silhoutte' A self portrait captured during sunset at Laguna by Alliah.</div>
+						</div>
+
+						<div class="gallery">
+						  <a target="_blank" href="f2.jpg">
+						    <img src="f2.jpg" alt="calligraphy_03" width="300" height="200"></a>
+						  <div class="desc">'Drink Repeat Again' Captured at Buendia Food by The Court at Makati</div>
+						</div>
+
+						<div class="gallery">
+						  <a target="_blank" href="f3.jpg">
+						    <img src="f3.jpg" alt="calligraphy_04" width="300" height="200"></a>
+						  <div class="desc">Scenic view at seaside MOA.</div>
+						</div>
+
+						<div class="gallery">
+						  <a target="_blank" href="f4.jpg">
+						    <img src="f4.jpg" alt="calligraphy_05" width="300" height="200"></a>
+						  <div class="desc">The 'MOA Eye'</div>
+						</div>
+				<div class="clearfix"></div>
+				</section>
+			
+		</main>
+		<div class="clearfix"></div>
+		<section>
+	<footer>ARB &copy; 2019 by Alliah Rose Bautista</footer>
+		</section>
+</body>
+</html>
---
+++ userdes1-lab1/gigs-port.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Photog Gigs</title>
+	<meta charset="UTF-8">
+	<link rel="icon" href="fav.png">
+	<link rel="shortcut icon" href="fav.png">
+	<link type="text/css"  rel="stylesheet"  href="style.css">
+</head>
+<body>
+	<header>
+	<div id="logo"><img src="fav.png" alt="logo"></div>
+		<nav>
+	        <ul>
+    	        <li><a href="index.html">H O M E</a></li>
+        	    <li><a href="about.html">A B O U T</a></li>
+            	<li><a href="photography.html">P H O T O G R P H Y</a></li>
+            	<li><a href="blog.html">B L O G S</a></li>
+            	<li><a href="contact.html">C O N T A C T</a></li>
+    	</nav>
+	</header>
+	<div class="clearfix"></div>		
+	<main>
+			<!-- HOME-->
+				<section id="index.htm">
+						<h2>PORTFOLIO</h2>
+					
+				</section>
+			<!-- HEADER UNDER LOGO -->
+				<section class="span4 offset4">
+					<img src="divider_1.png" class="left span3 offset1">
+					<h1 class="left span4">       G I G S</h1>
+					<img src="divider_1.png" class="left span3 offset1">
+					<div class="clearfix"></div>
+					<p>
+						Photos taken during gigs of Gracenote and Imago. Captured by yours truly. 
+					</p>
+				</section>
+			<!-- CONTENT 01 -->
+				<section class="offset2">
+			<!--GALLERY -->
+						<div class="gallery">
+						  <a target="_blank" href="G7.jpg">
+						    <img src="G7.jpg" alt="Drawings_01" width="300" height="200">
+						  </a>
+						  <div class="desc">A portrait of Jazz Jorge of Gracenote band.</div>
+						</div>
+
+						<div class="gallery">
+						  <a target="_blank" href="g8.jpg">
+						    <img src="g8.jpg" alt="Drawings_02" width="600" height="400">
+						  </a>
+						  <div class="desc">A portrait of Kiara San Luis of Imago band.</div>
+						</div>
+
+						<div class="gallery">
+						  <a target="_blank" href="g9.jpg">
+						    <img src="g9.jpg" alt="Drawing_03" width="600" height="400">
+						  </a>
+						  <div class="desc">A portrait of Eunice Jorge from Gracenote Band, playing a violin. </div>
+						</div>
+
+						<div class="gallery">
+						  <a target="_blank" href="g10.jpg">
+						    <img src="g10.jpg" alt="Drawing_04" width="600" height="400">
+						  </a>
+						  <div class="desc">A portrait of Jazz Jorge of Gracenote band.</div>
+						</div>
+						<div class="gallery">
+						  <a target="_blank" href="g11.jpg">
+						    <img src="g11.jpg" alt="Drawing_05" width="600" height="400">
+						  </a>
+						  <div class="desc">A portrait f Eunice Jorge, vocalist of Gracenote band.</div>
+						</div>
+				<div class="clearfix"></div>
+				</section>
+				<!-- CONTACT -->
+				<section class="span4 offset4">
+					<a href="https://www.facebook.com/arbautistaaa"><img src="fb-icon.png" class="offset5 left span1" alt="FACEBOOK"></a>
+					<a href="https://instagram.com/arbautistaaa?igshid=sos19hnge4vh"><img src="insta-icon.png" class="left span1" alt="INSTAGRAM"></a>
+					<a href="https://twitter.com/arbautistaaa"><img src="twitter-icon.png" class="left span1" alt="TWITTER"></a>
+					
+				</section>
+		</main>
+		<div class="clearfix"></div>
+		<section>
+	<footer>ARB &copy; 2019 by Alliah Rose Bautista</footer>
+		</section>
+</body>
+</html>
